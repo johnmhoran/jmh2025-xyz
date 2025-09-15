431ab46d0e019556ac5606da23aa758064a42920
johnmhoran <johnmhoran@gmail.com>
2025-09-14 23:07:15 -0700

    Update footer.js
    Signed-off-by: johnmhoran <johnmhoran@gmail.com>

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -9,7 +9,7 @@
 const now = new Date().toISOString().replace("T", " ").split(".")[0] + " UTC";
 document.getElementById("footer").innerHTML = `
 <footer class="bg-dark text-light text-center py-3 mt-auto">
-  Copyright © _____   |   License: _____   |   Built with TLC<br>
+  Copyright © _____ &nbsp;&nbsp; | &nbsp;&nbsp; License: _____ &nbsp;&nbsp; | &nbsp;&nbsp; Built with TLC<br>
   Last deployed: ${now}
 </footer>
 `;
